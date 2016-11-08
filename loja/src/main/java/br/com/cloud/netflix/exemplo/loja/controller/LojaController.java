396c1c1942681f1acf5c02df515019d2265e72a3
Vicente Neto <sneto.vicente@gmail.com>
2016-11-08 01:04:44 -0300

    Creating Loja service

---
+++ loja/src/main/java/br/com/cloud/netflix/exemplo/loja/controller/LojaController.java
@@ -0,0 +1,25 @@
+package br.com.cloud.netflix.exemplo.loja.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cloud.netflix.exemplo.loja.domain.Loja;
+
+@RestController
+public class LojaController {
+
+	private static List<Loja> lojas = new ArrayList<Loja>();
+
+	public LojaController() {
+		lojas.add(new Loja("123", "Loja A"));
+		lojas.add(new Loja("456", "Loja B"));
+	}
+
+	@RequestMapping("/")
+	public List<Loja> getRaces() {
+		return lojas;
+	}
+}
