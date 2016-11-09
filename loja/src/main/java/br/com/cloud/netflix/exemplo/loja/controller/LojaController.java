abe524e29f2cdbe5afec74122cb348cb18067451
Vicente Neto <sneto.vicente@gmail.com>
2016-11-08 23:50:48 -0300

    Configuring a Hystrix CircuitBreak on lojas service

--- loja/src/main/java/br/com/cloud/netflix/exemplo/loja/controller/LojaController.java
+++ loja/src/main/java/br/com/cloud/netflix/exemplo/loja/controller/LojaController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cloud.netflix.exemplo.loja.domain.Loja;
 import br.com.cloud.netflix.exemplo.loja.domain.LojaComClientes;
-import br.com.cloud.netflix.exemplo.loja.feign.ClientesClient;
+import br.com.cloud.netflix.exemplo.loja.hystrix.ClientesBean;
 
 @RestController
 public class LojaController {
 
 	@Autowired
-	private ClientesClient clientesClient;
+	private ClientesBean clientesBean;
 
 	private static List<Loja> lojas = new ArrayList<Loja>();
 
@@ -34,7 +34,7 @@ public class LojaController {
 		List<LojaComClientes> returnLojas = new ArrayList<LojaComClientes>();
 
 		for (Loja loja : lojas) {
-			returnLojas.add(new LojaComClientes(loja, clientesClient.getClientes(loja.getId())));
+			returnLojas.add(new LojaComClientes(loja, clientesBean.getClientes(loja.getId())));
 		}
 
 		return returnLojas;
